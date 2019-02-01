1e04e8131473c753c12858932e4c258fde34f9a9
Justin Marsella <marsellj@sheridancollege.ca>
2019-02-01 14:32:36 -0500

    My code that I am supposed to edit

--- src/ca/sheridancollege/week3/softwarefundamentals/ice1/CardTrick.java
+++ src/ca/sheridancollege/week3/softwarefundamentals/ice1/CardTrick.java
@@ -10,7 +10,9 @@ package ca.sheridancollege.week3.softwarefundamentals.ice1;
  * and then asks the user to pick a card and searches the array of cards
  * for the match to the user's card. To be used as starting code in ICE 1
  * @author Megha
+ * @co-author Justin
  */
+import java.util.Scanner;
 public class CardTrick {
     
     public static void main(String[] args)
@@ -20,10 +22,28 @@ public class CardTrick {
         for (int i=0; i<magicHand.length; i++)
         {
             Card c = new Card();
-            c.setValue((int)Math.random() * (13 - 1) + 1);
-            c.setSuit(Card.SUITS[(int)Math.random() * (3 - 0) + 0]);
+            c.setValue((int)(Math.random() * ((13 - 1) + 1)) + 1);
+            c.setSuit(Card.SUITS[(int)(Math.random() * ((3 - 0) + 1)) + 0]);
+            System.out.println(c.getValue());
+            System.out.println(c.getSuit());
         }
-        
+        Scanner input = new Scanner(System.in);
+        System.out.println("Please enter the value.");
+        int value = input.nextInt();
+        System.out.println("Please enter the suit.");
+        String suit = input.next();
+        for(int i = 0; i < magicHand.length; i++){
+            int v = magicHand[i].getValue();
+            String s = magicHand[i].getSuit();
+            if(value == v && suit.equalsIgnoreCase(s)){
+                System.out.println("The card is in the magic hand!");
+                break;
+            }
+            else{
+                System.out.println("The card is not in the magic hand...");
+                break;
+            }
+        }
     }
     
 }
